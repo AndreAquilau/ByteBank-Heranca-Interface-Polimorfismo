13735badc385534cc0a597b014b3f47bbdb4f23c
andreaquilau <andre.aquilau@outlook.com>
2021-08-17 00:41:56 -0400

    Adicionar arquivos de projeto.

---
+++ Byte-Bank-OOP/Funcionarios/Desenvolvedor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Desenvolvedor : Funcionario
+    {
+        public Desenvolvedor(string cpf): base(2000.00, cpf: cpf) {
+            Console.WriteLine("Criando Desenvolvedor");
+        }
+
+        public override double GetBonificacao()
+        {
+            return base.Salario * .1;
+        }
+
+        public override void AumentarSalario()
+        {
+            base.Salario *= .15;
+        }
+    }
+}
---
+++ Byte-Bank-OOP/Funcionarios/Diretor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Sistemas;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : FuncionarioAutenticavel
+    {
+        public Diretor(string cpf) : base(5000.00, cpf: cpf)
+        {
+            Console.WriteLine("Criando Diretor");
+        }
+
+        public override double GetBonificacao()
+        {
+            return base.Salario * 0.5;
+        }
+
+        public override void AumentarSalario()
+        {
+            base.Salario *= 1.15;
+        }
+    }
+}
